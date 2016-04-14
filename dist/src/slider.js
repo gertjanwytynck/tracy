c77ce14d88adaf59e8961691ce781883b72c658e
Jonatan Michiels <jonatan@Gertjans-MacBook-Pro.local>
2016-04-14 12:49:29 +0200

    feat(random-images): added random images script

---
+++ dist/src/slider.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    // CONSTANT VARS
+    const imageCount = 5;
+    const path = "img/slider/";
+
+    // RUN
+    run(createArrayImages(path, imageCount), generateRandomNumber(imageCount));
+});
+
+// HELPER FUNCTIONS
+var createArrayImages = function(path, imageCount) {
+  var images = [];
+  for (var i = 1; i <= imageCount; i++) {
+    images.push(path + "img" + i + ".jpg");
+  }
+  return images;
+}
+
+var generateRandomNumber = function(imageCount) {
+  return Math.ceil(Math.random() * imageCount);
+}
+
+var run = function(images, random) {
+   $("body").css("background-image", "url(" + images[random - 1] + ")");
+}
